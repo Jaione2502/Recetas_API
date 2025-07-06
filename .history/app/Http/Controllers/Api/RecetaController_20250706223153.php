3256c202fae40cfa6721c1ccd68146e186de3e85
Josemi1606 <josemiguelsalinasnicolas@gmail.com>
2025-07-06 23:16:21 +0200

    cambios 06/07

--- .history/app/Http/Controllers/Api/RecetaController_20250706223153.php
+++ .history/app/Http/Controllers/Api/RecetaController_20250706223153.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
-use App\Http\Controllers\Controller;
 
 class RecetaController extends Controller
 {
