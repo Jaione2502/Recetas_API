a4e300c1826891d2ea72750b720c5c5011b4bce5
Jaione2502 <jaldaregia@tigloo.es>
2025-09-09 13:49:00 +0200

    cambios TFM final
    cambios TFM final

--- routes/api.php
+++ routes/api.php
@@ -18,16 +18,30 @@ Route::get('/recetas/{id}', [RecetaController::class, 'show']);
 Route::get('/dietas', [DietaController::class, 'index']);
 Route::get('/dietas/{id}', [DietaController::class, 'show']);
 
+
+
 // RUTAS PRIVADAS
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('categoria', CategoriaController::class);
-    Route::apiResource('comentario', ComentarioController::class);
-    Route::apiResource('usuario', UsuarioController::class);
+  Route::apiResource('categoria', CategoriaController::class);
+   Route::apiResource('comentario', ComentarioController::class);
+   Route::apiResource('usuario', UsuarioController::class);
     Route::apiResource('inventario', InventarioController::class);
     Route::apiResource('menus', MenuController::class);
     Route::apiResource('ingredientes', IngredienteController::class);
-    Route::apiResource('recetas', RecetaController::class, ['except' => ['index', 'show']]);
-    Route::apiResource('lineas', LineaRecetaController::class);
-    Route::apiResource('dietas', DietaController::class, ['except' => ['index', 'show']]);
+
+    Route::post('/recetas', [RecetaController::class, 'store']);
+    Route::put('/recetas/{id}', [RecetaController::class, 'update']);
+    Route::delete('/recetas/{id}', [RecetaController::class, 'destroy']);
+
+    Route::get('/lineas', [LineaRecetaController::class, 'index']);
+    Route::get('/lineas/{id}', [LineaRecetaController::class, 'show']);
+    Route::post('/lineas', [LineaRecetaController::class, 'store']);
+    Route::put('/lineas/{id}', [LineaRecetaController::class, 'update']);
+    Route::delete('/lineas/{id}', [LineaRecetaController::class, 'destroy']);
+
+    Route::post('/dietas', [DietaController::class, 'store']);
+    Route::put('/dietas/{id}', [DietaController::class, 'update']);
+    Route::delete('/dietas/{id}', [DietaController::class, 'destroy']);
+
     Route::post('/logout', [UsuarioController::class, 'logout']);
 });
